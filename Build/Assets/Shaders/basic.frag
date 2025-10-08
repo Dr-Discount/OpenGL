bc3e35b4fb6ec1a870441959bf0b9a81377a6ac7
Dr-Discount <trackerjack02@gmail.com>
2025-10-08 10:58:47 -0600

    vertex shader

---
+++ Build/Assets/Shaders/basic.frag
@@ -0,0 +1,8 @@
+#version 460 core
+
+//in vec2 ocolor;
+out vec4 fcolor;
+
+void main() {
+	fcolor = vec4(1, 0, 0, 1);
+}
--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char* argv[]) {
 
     SDL_Event e;
     bool quit = false;
-
+    
+	//openGL setup
     std::vector<neu::vec3> points {
         {-0.5f, 0.3f, 0 },
         {0.9f, 0.7f, 0 },
@@ -23,6 +24,28 @@ int main(int argc, char* argv[]) {
         {0, 0, 1},
         {0, 1, 0}
 	};
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, points.size() * sizeof(neu::vec3), points.data(), GL_STATIC_DRAW);
+
+    GLuint vao;
+	glGenVertexArrays(1, &vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+    //vertex shader
+    std::string vs_source;
+	neu::file::ReadTextFile("Shaders/Basic.vert", vs_source);
+	const char* vs_cstr = vs_source.c_str();
+    
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vs_cstr, nullptr);
+	glCompileShader(vs);
 
     // MAIN LOOP
     while (!quit) {
